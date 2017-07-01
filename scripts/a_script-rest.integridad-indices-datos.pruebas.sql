cac96365c5dba7c9b62b7f807615e5f426e725dd
Guzmán López <guzilop@gmail.com>
2017-07-01 20:11:05 -0300

    first commit

---
+++ scripts/a_script-rest.integridad-indices-datos.pruebas.sql
@@ -0,0 +1,7 @@
+/*
+*****************************************************************************************
+Script con las restricciones de integridad creadas sobre el script de creación de tablas,
+índices, y el ingreso de datos de prueba.
+*****************************************************************************************
+*/
+
