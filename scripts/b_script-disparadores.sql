7fd02de7723db965e05887d4cf0efa4be5ead6d0
Guzmán López <guzilop@gmail.com>
2017-07-02 23:02:21 -0300

    script b: agregar archivo sql con estructura de preguntas

---
+++ scripts/b_script-disparadores.sql
@@ -0,0 +1,45 @@
+/*
+*********************************************************************************************
+* Script con disparadores                                                                   *
+*********************************************************************************************
+*/
+USE BD_VEHICULOS;
+go
+/*
+*********************************************************************************************
+* a. Crear un trigger que valide un número de VIN al ingresar un nuevo vehículo, debe
+* utilizar la función creada en el punto 4a.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* b. Crear un trigger al ingresar una línea de carga, ponga el peso correcto de acuerdo al
+* peso del vehículo mas el 5% de combustible y accesorios, también debe acumular el
+* peso total del envío.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* c. Definir un trigger que al ingresar un envío, si el país de origen es igual al país de
+* destino no procese la linea.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* d. Implementar un trigger que permita el borrado de uno o más envíos, para ello debe
+* borrar en cascada todos los registros en las tablas relacionadas, se debe tener en
+* cuenta los borrados múltiples.
+*********************************************************************************************
+*/
+
+
+
+
+
+
