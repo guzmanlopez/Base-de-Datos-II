abc5e77cc50454ae51d1d9931ead9e1073beed7c
Guzmán López <guzilop@gmail.com>
2017-07-02 23:19:53 -0300

    script d: comenzar script consultas y vistas

---
+++ scripts/d_script-consultas-vista.sql
@@ -0,0 +1,92 @@
+/*
+*********************************************************************************************
+* Script con consultas y vista                                                              *
+*********************************************************************************************
+*/
+USE BD_VEHICULOS;
+go
+
+/*
+*********************************************************************************************
+* a. Mostrar el mayor peso, el menor peso, la mayor fecha y la menor fecha de los envíos
+* de los años 2015 y 2016.
+*********************************************************************************************
+*/
+
+/*
+*********************************************************************************************
+* b. Mostrar para cada fabricante su nombre, la cantidad de vehículos enviados y el peso
+* total de los mismos en el año 2016 (ordenar los resultados de mayor peso a menor
+* peso).
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* c. Para todos los países que fueron destino de envíos, mostrar su nombre, su cantidad
+* de envíos en los 20 primeros días de enero de 2016 y la fecha del último envío
+* realizado, si algún país nunca fue destino de envíos igual debe mostrar su nombre.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* d. Mostrar los datos de los fabricantes que tengan envíos con más de 500 vehículos y
+* que no tengan más de 3 envíos con menos de 100 vehículos.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* e. Obtener todos los datos de los vehículos que fueron enviados en la fecha más
+* reciente.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* f. Mostrar los datos de los fabricantes que no han hecho envíos en el primer semestre
+* del 2016, registraron algún envío en el 2017 y solo han hecho envíos con destino
+* Holanda.
+*********************************************************************************************
+*/
+
+
+
+/*
+*********************************************************************************************
+* g. Para cada vehículo con menos de 2.300 kg de peso, mostrar sus datos, la fecha del
+* último envío donde participó, y el nombre del fabricante.
+*********************************************************************************************
+*/
+
+
+
+/*
+*********************************************************************************************
+* h. Bajar el peso en un 5% a todos los vehículos que no nunca fueron enviados pasado 1
+* año de su fabricación (puede utilizar la función 4b).
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* i. Obtener los datos de los fabricantes de Japón que en el 2016 hayan tenido en algún
+* mes un envío de cantidad total superior a 100 vehículos y que también hayan tenido
+* algún mes con una cantidad total inferior a 20.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* 7. Vistas:.
+* Realizar una vista que muestre País de Fabricación del vehículo y cantidad de vehículos
+* enviados a Países diferentes a los de la fábrica
+*********************************************************************************************
+*/
