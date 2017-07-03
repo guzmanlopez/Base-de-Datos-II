f88ea795c80726a74350fd254b1ed4398a4ee7f1
Guzmán López <guzilop@gmail.com>
2017-07-02 23:06:52 -0300

    script c: agregar archivo sql con estructura de preguntas

---
+++ scripts/c_script-funciones-procedimientos.sql
@@ -0,0 +1,61 @@
+/*
+*********************************************************************************************
+* Script con funciones y procedimientos                                                     *
+*********************************************************************************************
+*/
+USE BD_VEHICULOS;
+go
+/*
+*********************************************************************************************
+* a. Crear una función almacenada que reciba como parámetro un número de VIN
+* completo y valide el dígito verificador, si dicho dígito es correcto la función debe
+* retornar el texto ‘OK’, si es incorrecto, debe retornar todo el número de VIN pero
+* incluyendo el dígito verificador correcto.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* b. Crear una función que reciba como parámetro un VIN y retorne el año del modelo de
+* dicho vehículo.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* c. Dado un rango de fechas, retornar la cantidad de vehículos que recibió el país que
+* más vehículos recibió en dicho rango.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* d. Mediante un procedimiento almacenado que reciba un rango de fechas de envío,
+* cambiar el país de destino del envío a todos los vehículos cuyo país de fabricación del
+* vehículo sea el mismo que el país de destino del envío, en ese caso crear un país de
+* código ‘#’ que tenga como descripción ‘LOCAL’ y poner ese dato como destino.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* e. Dado un rango de fechas, implementar una función que retorne el nombre del país al
+* cual se le envió la menor cantidad de vehículos en dicho rango.
+*********************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************
+* f. Realizar un procedimiento almacenado que reciba un código de fabricante y retorne
+* con parámetro de salida la cantidad de plantas que tiene y la cantidad de modelos de
+* vehículos diferentes que produce.
+*********************************************************************************************
+*/
+
+
+
