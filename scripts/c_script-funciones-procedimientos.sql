ca11e2a7221b43e7abddba6c81201db6fdc421cc
Guzmán López <guzilop@gmail.com>
2017-07-03 10:58:59 -0300

    script c: retornar valores numericos asociados

--- scripts/c_script-funciones-procedimientos.sql
+++ scripts/c_script-funciones-procedimientos.sql
@@ -13,16 +13,148 @@ go
 * incluyendo el dígito verificador correcto.
 *********************************************************************************************
 */
-CREATE FUNCTION funct_validar_digitoverificador_vin
+
+ALTER FUNCTION funct_valor_num_asociado_vin
+(@letravin character(1))
+RETURNS character(1)
+AS
+BEGIN
+DECLARE @ret character(1)
+
+IF(@letravin = 'A') 
+	BEGIN
+	SET @ret = 1
+	END
+ELSE IF(@letravin = 'B') 
+	BEGIN
+	SET @ret = 2
+	END
+ELSE IF(@letravin = 'C') 
+	BEGIN
+	SET @ret = 3
+	END
+ELSE IF(@letravin = 'D') 
+	BEGIN
+	SET @ret = 4
+	END
+ELSE IF(@letravin = 'E') 
+	BEGIN
+	SET @ret = 5
+	END
+ELSE IF(@letravin = 'F') 
+	BEGIN
+	SET @ret = 6
+	END
+ELSE IF(@letravin = 'G') 
+	BEGIN
+	SET @ret = 7
+	END
+ELSE IF(@letravin = 'H') 
+	BEGIN
+	SET @ret = 8
+	END
+ELSE IF(@letravin = 'J') 
+	BEGIN
+	SET @ret = 1
+	END
+ELSE IF(@letravin = 'K') 
+	BEGIN
+	SET @ret = 2
+	END
+ELSE IF(@letravin = 'L') 
+	BEGIN
+	SET @ret = 3
+	END
+ELSE IF(@letravin = 'M') 
+	BEGIN
+	SET @ret = 4
+	END
+ELSE IF(@letravin = 'N') 
+	BEGIN
+	SET @ret = 5
+	END
+ELSE IF(@letravin = 'P') 
+	BEGIN
+	SET @ret = 7
+	END
+ELSE IF(@letravin = 'R') 
+	BEGIN
+	SET @ret = 9
+	END
+ELSE IF(@letravin = 'S') 
+	BEGIN
+	SET @ret = 2
+	END
+ELSE IF(@letravin = 'T') 
+	BEGIN
+	SET @ret = 3
+	END
+ELSE IF(@letravin = 'U') 
+	BEGIN
+	SET @ret = 4
+	END
+ELSE IF(@letravin = 'V') 
+	BEGIN
+	SET @ret = 5
+	END
+ELSE IF(@letravin = 'W') 
+	BEGIN
+	SET @ret = 6
+	END
+ELSE IF(@letravin = 'X') 
+	BEGIN
+	SET @ret = 7
+	END
+ELSE IF(@letravin = 'Y') 
+	BEGIN
+	SET @ret = 8
+	END
+ELSE IF(@letravin = 'Z') 
+	BEGIN
+	SET @ret = 9
+	END
+ELSE
+	BEGIN
+	SET @ret = 0
+	END
+
+RETURN @ret
+END;
+
+
+ALTER FUNCTION funct_validar_digitoverificador_vin
 (@vin character(17))
 RETURNS character(17)
 AS
 BEGIN
 DECLARE @ret character(17)
+DECLARE @valAsociados varchar(17)
 
-
+-- a. Encontrar el valor numérico asociado a cada letra en el VIN (las letras I, O, Q y Ñ no se permiten)
+DECLARE @ite int
+SET @ite = 1
+WHILE (@ite < 18)
+	BEGIN
+		IF(@ite = 1)
+			BEGIN
+			SET @valAsociados = dbo.funct_valor_num_asociado_vin(SUBSTRING(@vin,1,1))
+			END
+		IF(@ite > 1)
+			BEGIN
+			DECLARE @valNum character(1)
+			SET @valNum = dbo.funct_valor_num_asociado_vin(SUBSTRING(@vin,@ite,1))
+			SET @valAsociados = @valAsociados + @valNum
+			END
+SET @ite = @ite + 1
+	END
+SET @ret = @valAsociados
 RETURN @ret
 END;
+
+
+DECLARE @output character(17)
+SET @output = dbo.funct_validar_digitoverificador_vin('ABCDEFGHJKLMNPRST');
+PRINT @output
 
 
 /*
